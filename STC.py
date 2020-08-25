c8e7411bd36bcbb2a6e9f022cb7c83930aac5244
Gagan <gagan.databee@gmail.com>
2020-08-25 17:17:11 +0530

    initialized project

---
+++ STC.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+
+from RpiMotorLib import RpiMotorLib
+    
+#define GPIO pins
+GPIO_pins = (17, 27, 22) # Microstep Resolution MS1-MS3 -> GPIO Pin
+direction= 6       # Direction -> GPIO Pin
+step = 5      # Step -> GPIO Pin
+
+GPIO_pins_2 = (18,23,24)
+direction_2 = 12
+step_2 = 25
+
+# Initialize the pins
+mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
+mymotortest_2 = RpiMotorLib.A4988Nema(direction_2, step_2, GPIO_pins_2, "A4988")
+
+# motor driver
+#motor_go(clockwise, steptype, steps, stepdelay, verbose, initdelay)
+#(Full, Half, 1/4, 1/8, 1/16)
+t = True
+f = False
+
+clockwise = t
+steptype = "1/8"
+steps = 200#30steps
+mymotortest.motor_go(clockwise, steptype , steps , .01, False, .05)
+
+clockwise_2 = t
+steptype_2 = "1/8"
+steps_2 = 200#23steps
+mymotortest_2.motor_go(clockwise_2, steptype_2 , steps_2 , .01, False, .05)
+    
+# good practise to cleanup GPIO at some point before exit
+#GPIO.cleanup()
+
+exit()
---
+++ zoomctrl.py
@@ -0,0 +1,51 @@
+import cv2
+
+scale = 2
+
+cam = cv2.VideoCapture(0)
+#cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+#cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+x = 640
+y = 480
+
+while True:
+
+    ret_val, image = cam.read()
+    # get the webcam size
+    height, width, channels = image.shape
+    #print("height is ",height)
+    #print("width is",width)
+    # prepare the crop
+    #centerX, centerY = int(height/2), int(width/2)
+    #radiusX, radiusY = int(scale*height/100), int(scale*width/100)
+
+    #minX, maxX = centerX-radiusX, centerX+radiusX
+    #minY, maxY = centerY-radiusY, centerY+radiusY
+    cv2.imshow("original", image)
+    minX = int(x/scale)
+    maxX = int((x/scale)+(width-(width/scale)))
+    minY = int(y/scale)
+    maxY = int((y/scale)+(height-(height/scale)))
+    #cropped = image[minY:maxY, minX:maxX]
+    #resized_cropped = cv2.resize(cropped, (width, height))
+
+    #cv2.imshow('Zoomed', resized_cropped)
+
+    key = cv2.waitKey(1)
+
+    # if q entered whole process will stop
+    if key == ord('q'):
+        break
+
+    if key == ord('-'):
+        if scale > 1.1:
+            scale -= 0.1  # 
+    if  key == ord('+'):
+        if scale <10:
+            scale += 0.1
+
+    # add + or - 5 % to zoom
+
+    #print("scale is ", scale)
+
+cv2.destroyAllWindows()
